6d2d8fcf747f3ce11e2cf8b24e4b214429c68648
Albert <albertvalldeperas@gmail.com>
2021-12-03 19:32:25 +0100

    avs06 | PR2 | moves code to arbol file

--- arbol.c
+++ arbol.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUFFSIZE 10
+
+char** get_argv(const int height,const int width){
+
+  char** result = malloc(sizeof(char)*BUFFSIZE*3);
+  for (int i=0; i < 3; i++)
+    result[i] = malloc(sizeof(char)*BUFFSIZE);
+
+  result[0] = NULL;
+  sprintf(result[1], "%d", height);
+  sprintf(result[2], "%d", width);
+
+  return (char**)result;
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,9 +27,23 @@ int main(int argc, char *argv[])
     height = atoi(argv[1]); /* height */
     width = atoi(argv[2]); /* width */
 
-    /* tu codigo aqui */
+    /* init code */
+
+    // Create parent
+    if ((height > 1) && (fork() == 0)){
+      main(3,get_argv(height - 1, width));
+      return 0;
+    }
+
+    // Create son
+    if ((width > 1) && (fork() == 0)){
+      main(3,get_argv(1, width - 1));
+      return 0;
+    }
+
+    /* end code */
 
     printf("Soc el proces %d y meu pare es %d\n", getpid(), getppid());
-    sleep(1);
+    sleep(50);
     return 0;
 }
